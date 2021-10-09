db619bdbed2c118753667cb881f364d98d7e6ead
TaniyaSewwandi <taniyasewwandi33@gmail.com>
2021-10-09 11:35:58 +0530

    update_DeleteVaccine

--- src/IT20123840/DeleteVaccine.java
+++ src/IT20123840/DeleteVaccine.java
@@ -59,6 +59,7 @@ public class DeleteVaccine extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(95, 158, 160));
